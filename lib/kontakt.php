27bddcd53de800bbe180d30e0a32e1f42944391d
hightech <office@webserveri.info>
2013-01-25 15:39:04 +0100

    Update lib/kontakt.php

--- lib/kontakt.php
+++ lib/kontakt.php
@@ -12,7 +12,7 @@
 <p>Ukoliko želite da se priključite projektu, potrebno je poslati zahtev na bar dve gore navedene mail adrese. Vas zahtev će biti razmotren 
 u roku od 24 sata, nakon čega cete dobiti e-mail poruku sa svim uputstvima ...<br />
 <b>Razvojni tim:</b> hightech(Milutin Gavrilović), ZDroid(Zlatan Vasović), vladap (Vladimir Popadić), ludus, nemysis, kgb<br />
-Ukoliko želite online podrsku, predlažemo vam nas irc kanal <b>#webserveri</b> na <b>irc.freenode.net</b> serveru.<br />
+Ukoliko želite online podrsku, predlažemo vam nas irc kanal <b>webserveri</b> na <b>irc.freenode.net</b> serveru.<br />
 Ukoliko ne koristite irc klijenta, predlažemo vam : <br /><a href="http://webchat.freenode.net/?channels=webserveri">http://webchat.freenode.net/?channels=webserveri</a> 
 </p>
 </div>
