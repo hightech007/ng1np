5190f148867638b8a869b4ce49bcb9cda2e8a329
hightech <office@webserveri.info>
2013-02-08 07:50:21 -0800

    Create svg.php

---
+++ lib/svg.php
@@ -0,0 +1,22 @@
+<div class="dvanest kol">
+<p>
+<svg version="1.1"
+  width="120" height="120" 
+  xmlns="">
+  <defs>
+    <radialGradient id="circleGrad">
+      <stop offset="0%"   stop-color="#212121" />
+      <stop offset="80%" stop-color="#014298" />
+    </radialGradient>
+  </defs>
+
+  <ellipse fill="url(#circleGrad)" stroke="#000" cx="50%"
+  cy="50%" rx="50%" ry="50%">
+    <animate attributeName="rx" values="0%;50%;0%" dur="1s" 
+      repeatCount="indefinite" />
+    <animate attributeName="ry" values="0%;50%;0%" dur="1s" 
+      repeatCount="indefinite" />
+  </ellipse>
+</svg>
+</p>
+</div>
