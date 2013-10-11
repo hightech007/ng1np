769c3b7c709cc2221b3de2d528f851ff58b936e0
Zlatan Vasović <legospace9876@gmail.com>
2013-10-11 07:35:51 +0200

    'setovati' → 'postaviti'

--- engine.php
+++ engine.php
@@ -2,7 +2,7 @@
 $db = new mysqli('localhost','user','pass','base');
 	if ($db -> connect_errno) { die('Connect Error: '.$db -> connect_errno); }
 	if (!mysqli_set_charset($db, "utf8")) {
-		printf("Ne mogu setovati karaktere na utf8: %s\n", mysqli_error($db));
+		printf("Ne mogu postaviti karaktere na utf8: %s\n", mysqli_error($db));
 		} else { 
 			$up = (!empty($_GET['up'])?$_GET['up']:'default_upit');
 			$rezultat = $db->query("SELECT * FROM info WHERE title = '$up'");
