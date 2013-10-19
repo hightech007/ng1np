b2f7222cb09eae6b71c4866a1a5d3aa96be421c3
Zlatan Vasović <legospace9876@gmail.com>
2013-10-19 11:09:35 +0200

    Uklonjeni nepotrebni komentari

--- xcache/dole-kesh.php
+++ xcache/dole-kesh.php
@@ -1,8 +1,8 @@
 <?php
-// Keshira sadrzaj u fajl
+// Kešira sadžaj u datoteku
 $keshirano = fopen($keshfajl, 'w');
 fwrite($keshirano, ob_get_contents());
 fclose($keshirano);
-//ob_end_flush(); // Salje output browser-u
+// Šalje output browser-u
 if(extension_loaded('zlib')) {ob_end_flush();} 
 ?>
