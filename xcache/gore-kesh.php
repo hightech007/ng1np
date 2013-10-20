49200bbe003be42df0c2a90dcab91d60db5784f3
Zlatan Vasović <legospace9876@gmail.com>
2013-10-20 18:47:42 +0200

    Single quotes

--- xcache/gore-kesh.php
+++ xcache/gore-kesh.php
@@ -1,13 +1,13 @@
 <?php
-$url = $_SERVER["SCRIPT_FILENAME"];
+$url = $_SERVER['SCRIPT_FILENAME'];
 $break = Explode('/', $url);
 $fajl = $break[count($break) - 1];
-$keshfajl = 'xcache/cache/kesh-'.substr_replace($fajl ,"",-4).'.html';
+$keshfajl = 'xcache/cache/kesh-'.substr_replace($fajl ,'',-4).'.html';
 $keshtime = 18000;
 
 // Servira iz keša ako je mlađe od $keshtime
 if (file_exists($keshfajl) && time() - $keshtime < filemtime($keshfajl)) {
-  echo "<!-- Keširana kopija, generisana ".date('H:i', filemtime($keshfajl))." -->\n";
+  echo '<!-- Keširana kopija, generisana '.date('H:i', filemtime($keshfajl)).' -->\n';
   include($keshfajl);
   exit;
 }
