6b2e48bf4d510ef445ab198b6b00eeb3be836f02
Zlatan Vasović <legospace9876@gmail.com>
2013-10-23 22:04:05 +0200

    Clean up index.php

--- master/index.php
+++ master/index.php
@@ -1,21 +1,16 @@
 <?php 
 require_once 'set/master.class.php';
 
-
 $db = new DB();
 $db->konektuj('localhost', 'user', 'pass', 'base');
 
-$rezultat = $db->select("* FROM info LIMIT 0,5");
+$rezultat = $db->select('* FROM info LIMIT 0,5');
 
 
 while  ($row = $rezultat->fetch_object() ) {
-	echo "<pre>"; echo $row->title." ".$row->url; echo "</pre>";
-} 
+  echo '<pre>'; echo $row->title.' '.$row->url; echo '</pre>';
+}
 
-//echo "<pre>";
-//var_dump($rezultat);
-//echo "</pre>";
-
-$db->kraj(); 
+$db->kraj();
 
 ?>
