64d1e0b7b36c7378bd8e4ad2abcfef61b5bf86e5
Zlatan Vasović <legospace9876@gmail.com>
2013-10-30 22:43:47 +0100

    navodnici

--- master/set/index.php
+++ master/set/index.php
@@ -5,11 +5,11 @@ require_once 'set/master.class.php';
 $db = new DB();
 $db->konektuj('localhost', 'webserve_master', 'master=true', 'webserve_master');
 
-$rezultat = $db->select("* FROM info LIMIT 0,5");
+$rezultat = $db->select('* FROM info LIMIT 0,5');
 
 
 while  ($row = $rezultat->fetch_object() ) {
-  echo "<pre>"; echo $row->title." ".$row->url; echo "</pre>";
+  echo '<pre>'; echo $row->title.' '.$row->url; echo '</pre>';
 } 
 
 $db->kraj();
