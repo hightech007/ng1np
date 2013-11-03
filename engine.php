6cfb9bb6edfa5ab852ad0e2aa7a6466710cc64e0
Zlatan Vasović <legospace9876@gmail.com>
2013-11-03 22:58:16 +0100

    cleanup engine

--- engine.php
+++ engine.php
@@ -1,16 +1,20 @@
 <?php
 $db = new mysqli('localhost','user','pass','base');
-	if ($db -> connect_errno) { die('Connect Error: '.$db -> connect_errno); }
-	if (!mysqli_set_charset($db, "utf8")) {
-		printf("Ne mogu postaviti karaktere na utf8: %s\n", mysqli_error($db));
-		} else { 
-			$up = (!empty($_GET['up'])?$_GET['up']:'default_upit');
-			$rezultat = $db->query("SELECT * FROM info WHERE title = '$up'");
-			if ($rezultat->num_rows>0) {
-				while ($row = $rezultat -> fetch_object()) {
-					require_once 'setup/tpl/_engine.php';
-				}
-					} else { echo 'no result'; }
-	} 
+if ($db -> connect_errno) {
+  die('Connect Error: '.$db -> connect_errno);
+}
+if (!mysqli_set_charset($db, "utf8")) {
+  printf("Ne mogu postaviti karaktere na utf8: %s\n", mysqli_error($db));
+} else { 
+  $up = (!empty($_GET['up'])?$_GET['up']:'default_upit');
+  $rezultat = $db->query("SELECT * FROM info WHERE title = '$up'");
+  if ($rezultat->num_rows>0) {
+    while ($row = $rezultat -> fetch_object()) {
+      require_once 'setup/tpl/_engine.php';
+    }
+  } else {
+    echo 'no result';
+  }
+} 
 
 ?>
