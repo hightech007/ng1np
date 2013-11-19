78c60266a7fbc9747ec3f47e05d93141d0965f03
Zlatan Vasović <legospace9876@gmail.com>
2013-11-19 22:42:52 +0100

    - space

--- master/index.php
+++ master/index.php
@@ -7,7 +7,7 @@ $db->konektuj('localhost', 'user', 'pass', 'base');
 $rezultat = $db->select('* FROM info LIMIT 0,5');
 
 
-while  ($row = $rezultat->fetch_object() ) {
+while  ($row = $rezultat->fetch_object()) {
   echo '<pre>';
   echo $row->title.' '.$row->url;
   echo '</pre>';
