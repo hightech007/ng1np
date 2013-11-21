bd7ba28d5d0b6a9e24e625242c55e8d790de2bf1
Zlatan Vasović <legospace9876@gmail.com>
2013-11-21 22:10:43 +0100

    cleanup

--- xcache/dole-kesh.php
+++ xcache/dole-kesh.php
@@ -3,6 +3,5 @@
 $keshirano = fopen($keshfajl, 'w');
 fwrite($keshirano, ob_get_contents());
 fclose($keshirano);
-// Šalje output browser-u
-if(extension_loaded('zlib')) {ob_end_flush();} 
-?>
+// Šalje izlaz pregledaču
+if (extension_loaded('zlib')) { ob_end_flush(); } 
