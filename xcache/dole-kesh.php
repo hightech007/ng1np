89887f4d691ffc6354bf080d6bc59e68fcb27d53
Zlatan Vasović <legospace9876@gmail.com>
2013-11-22 22:31:16 +0100

    - \s

--- xcache/dole-kesh.php
+++ xcache/dole-kesh.php
@@ -4,4 +4,4 @@ $keshirano = fopen($keshfajl, 'w');
 fwrite($keshirano, ob_get_contents());
 fclose($keshirano);
 // Šalje izlaz pregledaču
-if (extension_loaded('zlib')) { ob_end_flush(); } 
+if (extension_loaded('zlib')) { ob_end_flush(); }
