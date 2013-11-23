f61f83834d79ceae2890c83ff8b13cfe1e0dbe26
Zlatan Vasović <legospace9876@gmail.com>
2013-11-23 21:26:28 +0100

    coding style

--- xcache/gore-kesh.php
+++ xcache/gore-kesh.php
@@ -7,7 +7,7 @@ $keshtime = 18000;
 
 // Servira iz keša ako je mlađe od $keshtime
 if (file_exists($keshfajl) && time() - $keshtime < filemtime($keshfajl)) {
-  echo '<!-- Keširana kopija, generisana '.date('H:i', filemtime($keshfajl)).' -->\n';
+  echo '<!-- Keširana kopija, generisana ' . date('H:i', filemtime($keshfajl)) . ' -->\n';
   include($keshfajl);
   exit;
 }
